949bc2fa0bb363081a6388b4af402fc94a86b4a5
李良军 <liliangjun88@gmail.com>
2013-09-15 23:29:40 +0800

    Update

--- kuaibao/kuaibao.R
+++ kuaibao/kuaibao.R
@@ -10,22 +10,36 @@ userbase <- 373
 #connect data
 
 library(RODBC)
+detach('packages:RODBC')
 conn <- odbcConnect('mysql_data')
 
-#message
-message <- sqlQuery(conn,'SELECT DATE(msgtime) as date ,COUNT(*) as messagenum  FROM message GROUP BY DATE(msgtime)')
+#messagenum
+message <- sqlQuery(conn,'SELECT DATE(msgtime) as date ,COUNT(*) as messagenum 
+                    FROM message GROUP BY DATE(msgtime)')
 message
+
+
+#user
+useractive <- sqlQuery(conn,'SELECT DATE(msgtime) as date ,
+                      COUNT(DISTINCT(fakeid)) as activeuser
+                       FROM  message GROUP BY DATE(msgtime)')
+useractive
+
+plot(useractive$date,useractive$activeuser)
+
 
 #fund
 fund <- sqlQuery(conn,"select date,profit,rate FROM fund order by updatetime asc")
 fund
 
 ##subscribe
-subscribe <- sqlQuery(conn, 'SELECT DATE(msgtime) AS date,COUNT(*) AS subscribe  FROM message WHERE event =\'subscribe\'  GROUP BY DATE(msgtime)')
+subscribe <- sqlQuery(conn, 'SELECT DATE(msgtime) AS date,COUNT(*) AS subscribe  
+                      FROM message WHERE event =\'subscribe\'  GROUP BY DATE(msgtime)')
 subscribe
 
 ##unsubscribe
-unsubscribe <- sqlQuery(conn, 'SELECT DATE(msgtime) AS date,COUNT(*) AS unsubscribe  FROM message WHERE event =\'unsubscribe\'  GROUP BY DATE(msgtime)')
+unsubscribe <- sqlQuery(conn, 'SELECT DATE(msgtime) AS date,COUNT(*) AS unsubscribe 
+                        FROM message WHERE event =\'unsubscribe\'  GROUP BY DATE(msgtime)')
 unsubscribe
 
 ##userincrease
@@ -36,20 +50,24 @@ userincrease
 usertotal <- cumsum(userincrease) + userbase;
 usertotal
 
-usermerge <- merge(subscribe,unsubscribe,c('date'))
+usermerge <- merge(merge(subscribe,unsubscribe,c('date')),useractive,c('date'))
 usermerge
 
+useractiverate <- useractive[2] / usertotal
+useractiverate
+
 #user
-user <- cbind(usermerge,usertotal=usertotal)
+user <- cbind2(usermerge,usertotal,useractiverate)
 user
-
-
+summary(user)
 
 ##fund 
 fund$date
 summary(fund)
 
 plot(fund$date, fund$profit,type='b')
+
+plot( user$useractiverate,user$date,type='b')
 
 boxplot(fund$profit)
 hist(fund$profit)
