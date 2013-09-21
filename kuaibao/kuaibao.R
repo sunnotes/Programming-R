0fb42e1e5f5f19423eea3ecd2c1a503645835810
李良军 <liliangjun88@gmail.com>
2013-09-21 23:17:02 +0800

    update kuaibao

--- kuaibao/kuaibao.R
+++ kuaibao/kuaibao.R
@@ -2,8 +2,8 @@
 
 #This file write for statistic and analysis data of Kuaibao
 
-install.packages("ggplot2")
-
+#install.packages("ggplot2")
+library(ggplot2)
 userbase <- 373
 
 
@@ -13,83 +13,90 @@ library(RODBC)
 detach('packages:RODBC')
 conn <- odbcConnect('mysql_data')
 
-#messagenum
-message <- sqlQuery(conn,'SELECT DATE(msgtime) as date ,COUNT(*) as messagenum 
-                    FROM message GROUP BY DATE(msgtime)')
-message
 
 
-#user
-useractive <- sqlQuery(conn,'SELECT DATE(msgtime) as date ,
-                      COUNT(DISTINCT(fakeid)) as activeuser
-                       FROM  message GROUP BY DATE(msgtime)')
-useractive
-
-plot(useractive$date,useractive$activeuser)
 
 
-#fund
-fund <- sqlQuery(conn,"select date,profit,rate FROM fund order by updatetime asc")
+#fund data analysis
+fund <- sqlQuery(conn,"select date,profit, FROM fund order by updatetime asc")
 fund
 
+#statistics
+plot(fund$date, fund$profit,type='b')
+
+p <- ggplot()+
+  geom_point(data=fund,mapping=aes(x=date,y=profit),colour = "green")+
+  geom_point(data=fund,mapping=aes(x=date,y=rate),colour = "red")
+print(p)  
+
+p <- ggplot()+
+  geom_point(data=fund,mapping=aes(x=date,y=profit),colour = "green")
+print(p) 
+
+p <- ggplot()+
+  geom_boxplot(data=fund,mapping=aes(x=date,y=profit),colour = "green")
+print(p) 
+
+
+###########user data analysis
 ##subscribe
 subscribe <- sqlQuery(conn, 'SELECT DATE(msgtime) AS date,COUNT(*) AS subscribe  
                       FROM message WHERE event =\'subscribe\'  GROUP BY DATE(msgtime)')
 subscribe
 
+user <- as.data.frame(subscribe)
+user
 ##unsubscribe
 unsubscribe <- sqlQuery(conn, 'SELECT DATE(msgtime) AS date,COUNT(*) AS unsubscribe 
                         FROM message WHERE event =\'unsubscribe\'  GROUP BY DATE(msgtime)')
 unsubscribe
 
+user$unsubscribe <- unsubscribe$unsubscribe
+user
+
 ##userincrease
 userincrease <- subscribe$subscribe -unsubscribe$unsubscribe
-userincrease
+user$userincrease <-userincrease
+user
 
 #usertotal
 usertotal <- cumsum(userincrease) + userbase;
-usertotal
-
-usermerge <- merge(merge(subscribe,unsubscribe,c('date')),useractive,c('date'))
-usermerge
-
-useractiverate <- useractive[2] / usertotal
-useractiverate
-
-#user
-user <- cbind2(usermerge,usertotal,useractiverate)
+user$usertotal <-usertotal
 user
+
+
+#statistics
+qplot(unsubscribe$date,unsubscribe$unsubscribe)
+
+##way1
+p <- ggplot(data=unsubscribe,mapping=aes(x=date,y=unsubscribe))
+p + geom_point()
+p + geom_point()+stat_smooth()
+
+##way2
+p <- ggplot()+
+  geom_point(data=subscribe,mapping=aes(x=date,y=subscribe))+
+  stat_smooth(data=subscribe,mapping=aes(x=date,y=subscribe))
+print(p)
+
+
+p <- ggplot()+
+  geom_point(data=subscribe,mapping=aes(x=date,y=subscribe),colour = "green")+
+  geom_point(data=unsubscribe,mapping=aes(x=date,y=unsubscribe),colour = "red")
+ 
+print(p)  
+
+
 summary(user)
 
 
 
-##fund 
-fund$date
-summary(fund)
 
 
 
 
-#statistic
-useractiveratetimeseries = ts(user[6])
-useractiveratetimeseries
-plot.ts(useractiveratetimeseries)
-
-plot(fund$date, fund$profit,type='b')
-
-plot( user$useractiverate,user$date,type='b')
-
-boxplot(fund$profit)
-hist(fund$profit)
-
-fund.profit <- scale(fund$profit)
-fund.profit
-plot(fund.profit)
-
-
-
-
-
-
-chinese <-'中文'
-chinese
+###########message data analysis
+#messagenum
+message <- sqlQuery(conn,'SELECT DATE(msgtime) as date ,COUNT(*) as messagenum 
+                    FROM message GROUP BY DATE(msgtime)')
+message
