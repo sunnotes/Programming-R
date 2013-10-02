8bad58c743074bdd19308ce05f9a93f4bbdc220f
李良军 <liliangjun88@gmail.com>
2013-10-03 02:15:00 +0800

    kuaibao data

--- kuaibao/dataobject.R
+++ kuaibao/dataobject.R
@@ -3,10 +3,10 @@
 library(RODBC)
 conn <- odbcConnect('mysql_data')
 
-kuaibao.usernumbase <- 506
+kuaibao.followersbase <- 506
 
 ###########fund data analysis
-kuaibao.fund <- sqlQuery(conn,"SELECT DATE,profit,rate,updatetime FROM fund ORDER BY updatetime ASC")
+kuaibao.fund <- sqlQuery(conn,"SELECT date,profit,rate,updatetime FROM fund ORDER BY updatetime ASC")
 dim(kuaibao.fund)
 head(kuaibao.fund)
 summary(kuaibao.fund)
@@ -18,20 +18,21 @@ kuaibao.follower <- sqlQuery(conn , "SELECT  b.day,
     SUM(IF(b.type = \'unsubscribe\',b.num , 0)) AS unfollower 				
     FROM (SELECT DATE(msgtime) AS DAY ,type_detail AS TYPE ,COUNT(*) AS num FROM message a WHERE  msg_type = 5 GROUP BY DATE(a.msgtime),a.type_detail) b 
     GROUP BY b.day")
-kuaibao.follower$addfollower <- kuaibao.follower$newfollower - kuaibao.follower$unfollower
+kuaibao.follower$increasedfollower <- kuaibao.follower$newfollower - kuaibao.follower$unfollower
+kuaibao.follower$totalfollower <- cumsum(kuaibao.follower$increasedfollower) + kuaibao.followersbase;
 dim(kuaibao.follower)
 head(kuaibao.follower)
 summary(kuaibao.follower)
 
 
-###########messageS data analysis
+###########messages data analysis
 kuaibao.messages <- sqlQuery(conn,'
-    SELECT   id, userid, sex,create_time ,type_detail,content
-    FROM 
-  	message ',nullstring = NA_character_, na.strings = "NA")
+    SELECT   id, userid, sex,msgtime ,date(msgtime) as day,hour(msgtime) as hour,msg_type,type_detail,content
+  FROM 
+	message WHERE DATE(msgtime)>\'2013-07-22\' ',nullstring = NA_character_, na.strings = "NA")
 dim(kuaibao.messages)
 head(kuaibao.messages)
 summary(kuaibao.messages)
 
 
-save(kuaibao.follower,kuaibao.fund,kuaibao.messages,kuaibao.usernumbase,file ='kuaibao/kuaibao.RData')
+save(kuaibao.follower,kuaibao.fund,kuaibao.messages,file ='kuaibao/kuaibao.RData')
