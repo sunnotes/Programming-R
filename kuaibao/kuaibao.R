8c0e2cb488d54f4c4668135634025dffbf1a3396
李良军 <liliangjun88@gmail.com>
2013-10-03 02:15:09 +0800

    update

--- kuaibao/kuaibao.R
+++ kuaibao/kuaibao.R
@@ -4,132 +4,70 @@
 
 #install.packages("ggplot2")
 library(ggplot2)
-userbase <- 506
+load(file ='kuaibao/kuaibao.RData')
 
 
-#connect data
+opar <- par(no.readonly = TRUE)
+par(mfrow=c(2,3))
+par(opar)
 
-library(RODBC)
-conn <- odbcConnect('mysql_data')
+#fund statistics
+plot(kuaibao.fund$date, kuaibao.fund$profit,type='b')
 
-
-
-
-
-#fund data analysis
-fund <- sqlQuery(conn,"select date,profit, FROM fund order by updatetime asc")
-fund
-
-#statistics
-plot(fund$date, fund$profit,type='b')
-
-p <- ggplot()+
-  geom_point(data=fund,mapping=aes(x=date,y=profit),colour = "green")+
-  geom_point(data=fund,mapping=aes(x=date,y=rate),colour = "red")
+p <- ggplot(kuaibao.fund,aes(x=date,y=rate))+
+  geom_point(colour = "red")+
+  geom_smooth()
 print(p)  
 
-p <- ggplot()+
-  geom_point(data=fund,mapping=aes(x=date,y=profit),colour = "green")
+p <- ggplot(kuaibao.fund,aes(x=date,y=profit))+
+  geom_point(colour = "green")+
+  geom_smooth()
 print(p) 
 
-p <- ggplot()+
-  geom_boxplot(data=fund,mapping=aes(x=date,y=profit),colour = "green")
+p <- ggplot(kuaibao.fund,aes(x=date,y=profit))+
+  geom_boxplot(colour = "green")
 print(p) 
 
 
-###########user data analysis
-##subscribe
-subscribe <- sqlQuery(conn, 'SELECT DATE(msgtime) AS date,COUNT(*) AS subscribe  
-                      FROM message WHERE event =\'subscribe\'  GROUP BY DATE(msgtime)')
-subscribe
+###########follower data analysis
+##
 
-user <- as.data.frame(subscribe)
-user
-##unsubscribe
-unsubscribe <- sqlQuery(conn, 'SELECT DATE(msgtime) AS date,COUNT(*) AS unsubscribe 
-                        FROM message WHERE event =\'unsubscribe\'  GROUP BY DATE(msgtime)')
-unsubscribe
-
-user$unsubscribe <- unsubscribe$unsubscribe
-user
-
-##userincrease
-userincrease <- subscribe$subscribe -unsubscribe$unsubscribe
-user$userincrease <-userincrease
-user
-
-#usertotal
-usertotal <- cumsum(userincrease) + userbase;
-user$usertotal <-usertotal
-user
-
-
-#statistics
-qplot(unsubscribe$date,unsubscribe$unsubscribe)
-
-##way1
-p <- ggplot(data=unsubscribe,mapping=aes(x=date,y=unsubscribe))
-p + geom_point()
-p + geom_point()+stat_smooth()
-
-##way2
-p <- ggplot()+
-  geom_point(data=subscribe,mapping=aes(x=date,y=subscribe))+
-  stat_smooth(data=subscribe,mapping=aes(x=date,y=subscribe))
-print(p)
+head(kuaibao.follower)
+summary(kuaibao.follower)
 
 
 p <- ggplot()+
-  geom_point(data=subscribe,mapping=aes(x=date,y=subscribe),colour = "green")+
-  geom_point(data=unsubscribe,mapping=aes(x=date,y=unsubscribe),colour = "red")
- 
+  geom_point(data=kuaibao.follower,mapping=aes(x=kuaibao.follower$day,y=kuaibao.follower$newfollower),colour = "green")+
+  geom_point(data=kuaibao.follower,mapping=aes(x=kuaibao.follower$day,y=kuaibao.follower$increasedfollower),colour = "red") 
 print(p)  
-
-
-summary(user)
-
-
-qplot(height, data = message, geom = c("density"), 
-      facets = message.type ~ ., fill = message.type)
-
-
-
-
-
 
 
 ###########message data analysis
 
-##data
+head(kuaibao.messages)
+class(kuaibao.messages$msgtime)
+summary(kuaibao.messages$day)
 
-messagedata <- sqlQuery(conn,'
-SELECT   id, 
-	userid, 
-	sex,  
-	DATE(msgtime) AS date, HOUR(msgtime) AS hour,
-	type_detail
-	FROM 
-	test.message WHERE msgtime !=\'\' and msg_type =1 
-      and type_detail =\'compute\' or  type_detail =\'query\'                    
-                        ')
-
-head(messagedata)
-
-qplot(date,hour,data=messagedata,colour=type_detail,alpha = 0.01,geom='jitter')
+p<- ggplot(kuaibao.messages,aes(day))+
+  geom_bar(binwidth = 0.5,position = "stack")+
+  facet_grid(type_detail ~ .)
+p
+p<- ggplot(kuaibao.messages,aes(day))+
+  geom_bar(binwidth = 0.5,position = "stack")
+p
 
 
-table(messagedata$date,messagedata$hour,messagedata$type_detail)
+kuaibao.messages2 <- kuaibao.messages[kuaibao.messages$type_detail== 'query'||kuaibao.messages$type_detail== 'compute',]
+dim(kuaibao.messages2)
 
 
-#messagenum
+kuaibao.messages3 <- kuaibao.messages[kuaibao.messages$type_detail== 'query',]
+dim(kuaibao.messages3)
 
-message <- sqlQuery(conn,'SELECT DATE(msgtime) as date ,COUNT(*) as messagenum 
-                    FROM message GROUP BY DATE(msgtime)')
-message
+qplot(day,hour,data=kuaibao.messages,facet=factor(type_detail)~.,alpha = 0.01,geom='jitter')
 
 
-#message <- sqlQuery(conn,'SELECT * FROM message')
-#head(message)
+table(kuaibao.messages$day,kuaibao.messages$hour,kuaibao.messages$type_detail)
 
 
 
@@ -137,5 +75,8 @@ message
 
 
 
-
-
+z <- Sys.time()
+unclass(z)$hour
+mode(z)
+class(z)
+months(z)
