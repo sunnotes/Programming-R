5378110a78546259c9e56e8fbf0f4ce6d84a9427
sunnotes <liliangjun88@gmail.com>
2013-12-22 00:10:44 +0800

    modified

--- Data_Mining_with_R_Learning_by_Case_Studies/Predicting_Algae_Blooms.R
+++ Data_Mining_with_R_Learning_by_Case_Studies/Predicting_Algae_Blooms.R
@@ -63,10 +63,10 @@ algae[clicked.lines,]
 algae[algae$NH4 > 19000,]
 
 
-library(lattice)
+library('lattice')
+
 print(bwplot(size ~ a1, data=algae,ylab='River Size',xlab='Algal A1'))
 
-install.packages("Hmisc")
 library("Hmisc")
 print(bwplot(size ~ a1, data=algae,panel=panel.bpplot, 
              probs=seq(.01,.49,by=.01), datadensity=TRUE,
